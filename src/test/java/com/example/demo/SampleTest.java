1ffdf72c5ffa7e60dca12429fa7008d95661d494
jiren <yujienjoyfever@yahoo.co.jp>
2021-08-03 00:16:47 +0900

    _

---
+++ src/test/java/com/example/demo/SampleTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+}
